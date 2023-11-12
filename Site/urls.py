8fcd8b6d008587068c525b5771297307eb9cb1c0
minuguaxi <minuguaxi@gmail.com>
2023-11-11 23:11:53 -0300

    V0.2

--- Site/urls.py
+++ Site/urls.py
@@ -4,5 +4,10 @@ from Site import views
 
 urlpatterns = [
     path('', views.Index),
+    path('passagens/', views.passagens, name='passagens'),
+    path('pacotes/', views.pacotes, name='pacotes'),
+    path('ofertas/', views.ofertas, name='ofertas'),
+    path('alugueis/', views.alugueis, name='alugueis'),
+    path('seguros/', views.seguros, name='seguros'),
 ]
  
--- Site/views.py
+++ Site/views.py
@@ -3,3 +3,27 @@ from django.shortcuts import render
 
 def Index(request):
     return render(request, 'base.html')
+
+"' PASSAGENS '"
+def passagens(request):
+    return render(request, 'paginas/passagens.html')
+
+
+"' PACOTES '"
+def pacotes(request):
+    return render(request, 'paginas/pacotes.html')
+
+
+"' OFERTAS '"
+def ofertas(request):
+    return render(request, 'paginas/ofertas.html')
+
+
+"' ALUGUEIS '"
+def alugueis(request):
+    return render(request, 'paginas/alugueis.html')
+
+
+"' SEGUROS '"
+def seguros(request):
+    return render(request, 'paginas/seguros.html')
